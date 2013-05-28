7f5c41336618883f035434e75f0f5f5cdfba2a73
Satoru-Yamamura <satoru_yamamura@dwango.co.jp>
2013-05-28 16:20:46 +0900

    コメントを追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,33 +6,40 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-	  
+	//メインページにアクセス
     public static Result index() {
         return ok(index.render("メインページ"));
     }
     
+    //視聴ページにアクセス
     public static Result watch(String channelURI) {
         return ok(watch.render(channelURI));
     }
     
+    //コメントを投稿
     public static Result updateComment(String channelURI) {
+    	//視聴ページにリダイレクト
         return redirect("/watch/" + channelURI);
     }
     
+    //枠作成ページにアクセス
     public static Result newChannel() {
     	return ok(newChannel.render());
     }
 
+    //枠を作成
     public static Result updateChannel() {
     	String channelURI = "lv999999";
+    	//配信ページにリダイレクト
     	return redirect("/broadcast/" + channelURI);
     }
 
-    
+    //配信ページにアクセス
     public static Result broadcast(String channelURI) {
         return ok(broadcast.render(channelURI));
     }
     
+    //投稿者コメント
     public static Result updateOwnerComment(String channelURI) {
         return redirect("/broadcast/" + channelURI);
     }
