6ca3337ddc2179366b04d4ec9fbdcbcb5c1dd414
Satoru-Yamamura <satoru_yamamura@dwango.co.jp>
2013-05-28 16:12:30 +0900

    ページ遷移のひな形完成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,12 +11,31 @@ public class Application extends Controller {
         return ok(index.render("メインページ"));
     }
     
-    public static Result watch(String chURI) {
-        return ok(watch.render(chURI));
+    public static Result watch(String channelURI) {
+        return ok(watch.render(channelURI));
     }
     
-    public static Result updateComment(String chURI) {
-        return redirect("/watch/" + chURI);
+    public static Result updateComment(String channelURI) {
+        return redirect("/watch/" + channelURI);
     }
+    
+    public static Result newChannel() {
+    	return ok(newChannel.render());
+    }
+
+    public static Result updateChannel() {
+    	String channelURI = "lv999999";
+    	return redirect("/broadcast/" + channelURI);
+    }
+
+    
+    public static Result broadcast(String channelURI) {
+        return ok(broadcast.render(channelURI));
+    }
+    
+    public static Result updateOwnerComment(String channelURI) {
+        return redirect("/broadcast/" + channelURI);
+    }
+    
   
 }
