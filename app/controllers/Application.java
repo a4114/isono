b3e9b0be8bb0b8f49f0148b30093e2d0db4a8ec3
Satoru-Yamamura <satoru_yamamura@dwango.co.jp>
2013-05-28 15:24:35 +0900

    メインページと視聴ページを追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,17 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
+	  
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("メインページ"));
+    }
+    
+    public static Result watch(String chURI) {
+        return ok(watch.render(chURI));
+    }
+    
+    public static Result updateComment(String chURI) {
+        return redirect("/watch/" + chURI);
     }
   
 }
