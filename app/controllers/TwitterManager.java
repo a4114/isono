0c667e40fa2576c7c7cfe42ac116757ded4f6aed
kyohei_uchida <kyohei_uchida@dwango.co.jp>
2013-05-31 17:26:23 +0900

    ログイン済みか判別する方法 １．session().containsKey("TwitterID")が真 ２．User.userList.containsKey(session().get("TwitterID"))でUserインスタンスが取得できる 上記２つを試し、失敗したらloginにリダイレクトする
    shuusei

---
+++ app/controllers/TwitterManager.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import play.cache.Cache;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+public class TwitterManager {
+    static public final String CONSUMER_KEY = "tIRpAmBs6UtryMFGPL4wg";
+    static public final String CONSUMER_SECRET = "3Uak8Nk6euyBzxRsitDZTwrnSTZcZKTZlHe3WGwXmk";
+
+    
+    public static String getLoginUrl() throws TwitterException {
+        TwitterFactory twitterFactory = new TwitterFactory();
+        Twitter twitter = twitterFactory.getInstance();
+        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+
+        try {
+            RequestToken reqToken = twitter.getOAuthRequestToken();
+            Cache.set("RequestToken", reqToken);
+            Cache.set("TwitterForOAuth", twitter);
+            return reqToken.getAuthorizationURL();
+        } catch (TwitterException e) {
+            throw (e);
+        }
+
+    }
+
+/*    public static Twitter getTwitter() {
+        Twitter twitter = (Twitter) Cache.get("isonoTwitter");
+
+        try {
+            twitter.getScreenName();
+            return twitter;
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }*/
+
+    public static Twitter makeTwitter(String OauthVerifier)
+            throws TwitterException {
+        
+        Twitter twitter = (Twitter) Cache.get("TwitterForOAuth");
+
+        
+        if (twitter == null) {
+            throw new TwitterException("TwitterCache is null.");
+        }
+
+        try {
+            AccessToken accessToken = twitter
+                    .getOAuthAccessToken(OauthVerifier);
+            twitter.setOAuthAccessToken(accessToken);
+        } catch (TwitterException e) {
+            throw (e);
+        }
+
+        Cache.remove("RequestToken");
+        Cache.remove("TwitterForOAuth");
+        return twitter;
+    }
+
+}
